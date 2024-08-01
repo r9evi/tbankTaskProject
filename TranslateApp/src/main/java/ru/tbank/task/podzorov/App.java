0a98c1a6a6f436fb0b7f458cdaf5939a804b19d0
del <131190377+r9evi@users.noreply.github.com>
2024-08-01 14:03:23 +0300

    Initial commit

---
+++ TranslateApp/src/main/java/ru/tbank/task/podzorov/App.java
@@ -0,0 +1,13 @@
+package ru.tbank.task.podzorov;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
